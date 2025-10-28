9b78ae262b6905c9b27da5fe7a9d0c1f16589538
Boris Ryavkin <notoriskin@gmail.com>
2025-10-28 04:45:07 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+function createChristmasTree(levels, filepath) {
+    const fs = require('fs');
+    
+    let output = '';
+    const width = levels * 6; // примерная ширина елки
+
+    // Верхушка
+    output += ' '.repeat(width / 2) + 'W\n';
+    output += ' '.repeat(width / 2) + '*\n';
+
+    // "Этажи" ёлки
+    for (let i = 1; i <= levels; i++) {
+        let stars = '* '.repeat(i * 2 + 3);
+        let line = stars.trim();
+        if (i % 2 === 0) {
+            line = '@' + line + '@';
+        } else {
+            line = '*' + line + '*';
+        }
+        const padding = ' '.repeat((width - line.length) / 2);
+        output += padding + line + '\n';
+    }
+
+    // Ствол
+    for (let i = 0; i < 2; i++) {
+        const padding = ' '.repeat((width - 5) / 2);
+        output += padding + 'TTTTT\n';
+    }
+
+    fs.writeFileSync(filepath, output, 'utf-8');
+}
+
+// Пример использования:
+createChristmasTree(4, 'tree.txt');
+
